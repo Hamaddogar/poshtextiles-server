2cc8fc855477361717f350ef21f48838e7d6d3dc
Zain UL Abideen <42932321+Hamaddogar@users.noreply.github.com>
2023-02-27 21:27:15 +0500

    Create Header.jsx

---
+++ client/src/components/pages/dashboard/picking/inspection/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Box } from '@mui/system';
+import NavMenu from '../../../../header/NavMenu';
+import { Button } from '@mui/material';
+
+
+
+
+
+
+const Header = () => {
+
+    return (
+        <Box>
+            <NavMenu>
+                {/* <Button size='small' variant='contained' >History</Button> */}
+            </NavMenu>
+        </Box>
+    )
+}
+
+export default Header
