11d8d32a871c99441c211dd3478af400e0fa2e03
Hamad <42932321+Hamaddogar@users.noreply.github.com>
2023-07-04 06:04:19 -0700

    Update fakeData.js

--- client/src/RTK/Reducers/fakeData.js
+++ client/src/RTK/Reducers/fakeData.js
@@ -1,26 +1,3 @@
-
-import p1 from "../../components/assets/images/p1.png";
-import p2 from "../../components/assets/images/p2.png";
-import p3 from "../../components/assets/images/p3.png";
-import p4 from "../../components/assets/images/p4.png";
-import p5 from "../../components/assets/images/p5.png";
-import p6 from "../../components/assets/images/p6.png";
-import p7 from "../../components/assets/images/p7.png";
-import p8 from "../../components/assets/images/p8.png";
-import p9 from "../../components/assets/images/p9.png";
-import p10 from "../../components/assets/images/p10.png";
-import p11 from "../../components/assets/images/p11.png";
-import p12 from "../../components/assets/images/p12.png";
-import p13 from "../../components/assets/images/p13.png";
-import p14 from "../../components/assets/images/p14.png";
-import p15 from "../../components/assets/images/p0.png";
-
-
-
-
-
-
-
 export const orderDetail = {
     billTo: 'Posh Textiles, Inc.',
     projectName: 'Belmont Tower',
@@ -40,61 +17,7 @@ export const orderDetail = {
     discount: '$3000'
 }
 
-
-
-
-
-export const saleOrders_LineItems = [
-    { image: p1, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-001', status: true },
-    { image: p2, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-002', status: false },
-    { image: p3, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-003', status: false },
-    { image: p4, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-004', status: false },
-    { image: p5, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-005', status: false },
-    { image: p6, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-006', status: false },
-    { image: p7, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-007', status: false },
-    { image: p8, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-008', status: false },
-    { image: p9, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-009', status: false },
-    { image: p10, qty: 150, moq: 500, des: '100% IFR Polyester', id: 'P10001-010', status: false },
-    { image: p11, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-011', status: false },
-    { image: p12, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0012', status: false },
-    { image: p13, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0013', status: false },
-    { image: p14, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0014', status: false },
-    { image: p15, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0015', status: false },
-    { image: p2, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0016', status: false },
-    { image: p3, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0017', status: false },
-    { image: p4, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0018', status: false },
-    { image: p5, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0019', status: false },
-    { image: p6, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0020', status: false },
-    { image: p7, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-021', status: false },
-    { image: p8, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-022', status: false },
-    { image: p9, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-023', status: false },
-    { image: p10, qty: 150, moq: 500, des: '100% IFR Polyester', id: 'P10001-024', status: false },
-    { image: p11, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-025', status: false },
-    { image: p12, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-026', status: false },
-    { image: p13, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-027', status: false },
-    { image: p14, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-028', status: false },
-    { image: p15, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-029', status: false },
-    { image: p2, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-030', status: false },
-    { image: p3, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-031', status: false },
-    { image: p4, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-032', status: false },
-    { image: p5, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-033', status: false },
-    { image: p6, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-034', status: false },
-    { image: p7, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-035', status: false },
-    { image: p8, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-036', status: false },
-    { image: p9, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-037', status: false },
-    { image: p10, qty: 150, moq: 500, des: '100% IFR Polyester', id: 'P10001-038', status: false },
-    { image: p11, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-039', status: false },
-    { image: p12, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-040', status: false },
-    { image: p13, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-041', status: false },
-    { image: p14, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-042', status: false },
-    { image: p15, qty: 150, moq: 50, des: '100% IFR Polyester', id: 'P10001-0043', status: false },
-];
-
-
-export const allOrdersDefault = [];
-
-
-export const chargeCard = [
+export const chargeCardData = [
     { ono: '67891', name: 'Sefton Morrison', po: 'PO-000345', st: 'Pending', am: 2157, dt: '17-11-2022', cst: 'Authorized', },
     { ono: '67893', name: 'Hammad Morrison', po: 'PO-000345', st: 'Pending', am: 2157, dt: '17-11-2022', cst: 'Not Charged', },
     { ono: '67895', name: 'Sefton Morrison', po: 'PO-000345', st: 'Pending', am: 2157, dt: '17-11-2022', cst: 'Authorized', },
@@ -114,8 +37,6 @@ export const chargeCard = [
     { ono: '78916', name: 'Sefton Morrison', po: 'PO-000345', st: 'Pending', am: 2157, dt: '17-11-2022', cst: 'Declined', },
     { ono: '78920', name: 'Sefton Morrison', po: 'PO-000345', st: 'Pending', am: 2157, dt: '17-11-2022', cst: 'Declined', },
 ];
-
-
 
 export const stockDetails_Invoice = [
     { no: 'INV03042', dt: '11-08-2022', cn: 'JAGUAR HOSPITALITY', qty: 10, price: 35 },
@@ -163,8 +84,6 @@ export const stockDetails_Invoice = [
     { no: 'INV03042', dt: '11-08-2022', cn: 'JAGUAR HOSPITALITY', qty: 10, price: 35 },
     { no: 'INV03042', dt: '11-08-2022', cn: 'NATIONAL OFFICE SYSTEM', qty: 10, price: 35 },
 ];
-
-
 
 export const stockDetails_purchase = [
     { no: 'PO4666', vendorName: 'HANGZHOU ANTEXILES', orderDate: '11-08-2022', requestedDate: '11-08-2022', status: 'OPEN', eta: '01-15-23', amount: 247.32 },
@@ -259,7 +178,6 @@ export const stockDetails_ledger = [
     { eta: '11-20', entryType: 'New', postDate: 'Nov-20', itemName: 'JEROME SAHARA', lotNumber: 'LOT22182932', location: 'SYOSST', originalQty: 50, remainingQty: 29 },
     { eta: '11-20', entryType: 'New', postDate: 'Nov-20', itemName: 'JEROME SAHARA', lotNumber: 'LOT22182932', location: 'SYOSST', originalQty: 50, remainingQty: 29 },
 ];
-
 
 export const inventoryAdjustment = [
     { lotNo: 'LOT221838', itemName: 'TITAN-101', description: '100% Silk White', remainingQty: 35, adjQty: 10, price: 35 },
